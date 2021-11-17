1adbe547ee71b2236040e2669dfa71f081cb2b14
rileymiller28 <rileymiller28@icloud.com>
2021-11-16 21:26:13 -0600

    Added Server Connect to Q's

--- 375ProjectTeama8.appstudio/forms/questionaire/questionaire.js
+++ 375ProjectTeama8.appstudio/forms/questionaire/questionaire.js
@@ -3,6 +3,11 @@ let petName = ""
 let firstCar = ""
 let favMusic = ""
 
+let netID = "rjm73343"
+let datapass = "TopJon2005"
+let groupID = "375groupa8"
+let query = ""
+
 btnSubInfo.onclick=function(){
 
   btnsavepass.show()
@@ -17,12 +22,23 @@ let pwdLen = pass.length;
 let randPassword = Array(pwdLen).fill(pwdChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
   lblPassword.value = randPassword
   
-  
 }
-
 
 btnsavepass.onclick=function(){
   let hashPass = lblPassword.value
   console.log(hashPass)
 }
 
+query = "Select * From questionaire"
+ req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + datapass + "&database=" + groupID + "&query=" + query)
+ results = JSON.parse(req.responseText)
+var randomQ1 = results[Math.floor(Math.random()*results.length)];
+var randomQ2 = results[Math.floor(Math.random()*results.length)];
+var randomQ3 = results[Math.floor(Math.random()*results.length)];
+
+ 
+questionaire.onshow=function(){
+  iptPetName.header = randomQ1
+  iptFirstCar.header = randomQ2
+  iptFavMusic.header = randomQ3
+}
