241d4e78162423a488d1f06abd995c630ac58993
DaBob22 <90647821+DaBob22@users.noreply.github.com>
2021-11-23 09:52:02 -0600

    login3
    save

--- 375ProjectTeama8.appstudio/forms/homepage/homepage.js
+++ 375ProjectTeama8.appstudio/forms/homepage/homepage.js
@@ -1,20 +1,6 @@
 
 
-HTML
 
-<div class="scrolling-wrapper">
-  <div class="card"><h2>Card</h2></div>
-  <div class="card"><h2>Card</h2></div>
-  <div class="card"><h2>Card</h2></div>
-  <div class="card"><h2>Card</h2></div>
-  <div class="card"><h2>Card</h2></div>
-  <div class="card"><h2>Card</h2></div>
-  <div class="card"><h2>Card</h2></div>
-  <div class="card"><h2>Card</h2></div>
-  <div class="card"><h2>Card</h2></div>
-</div>
-
-
-End HTML
-
-
+Hamburger1.onclick=function(){
+  
+}
