dec04f996af9d7cfb11a13467d1d5f09174ea218
DaBob22 <90647821+DaBob22@users.noreply.github.com>
2021-11-22 18:46:34 -0600

    loginpage2
    loginpage

---
+++ 375ProjectTeama8.appstudio/forms/homepage/homepage.js
@@ -0,0 +1,20 @@
+
+
+HTML
+
+<div class="scrolling-wrapper">
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+  <div class="card"><h2>Card</h2></div>
+</div>
+
+
+End HTML
+
+
